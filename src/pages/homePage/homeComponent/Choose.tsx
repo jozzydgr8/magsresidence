cb392562763e817dbd6dceca256ad5a3146ff72c
jozzydgr8 <jozzydgreat1@gmail.com>
2026-07-29 15:22:19 -0700

    added image to choose

--- src/pages/homePage/homeComponent/Choose.tsx
+++ src/pages/homePage/homeComponent/Choose.tsx
@@ -1,5 +1,6 @@
 import { reason } from "../../../data"
-
+import roomImage from '../../../asset/room1.png'
+import roomImage2 from '../../../asset/room2.jpg'
 const iconStyle={
     fontSize:"1.5rem", color:"var(--teal)",
     background:'var(--transparent-gold)', borderRadius:'80%',
@@ -13,7 +14,7 @@ export const Choose = ()=>{
                 <div className="row">
                     <div className="col-md-6">
                         <p className="homeBadge">why choose us</p>
-                        <br/>
+                
                         <h2 className="subheading" style={{color:'var(--teal)'}}>The Mags Residence Difference</h2>
                         <br/>
                         <p className="sub-topic">
@@ -38,6 +39,42 @@ export const Choose = ()=>{
                             ))
                         }
                     </div>
+
+
+                    <div
+    className="col-md-6"
+    style={{
+        position: 'relative',
+        paddingBottom: '50px'
+    }}
+>
+    <div
+        style={{
+            backgroundImage: `url(${roomImage})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            minHeight: '600px',
+            borderRadius: '10px',
+            boxShadow: '0 10px 30px var(--secondary)'
+        }}
+    />
+
+    <div
+        style={{
+            position: 'absolute',
+            backgroundImage:`url(${roomImage2})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            bottom: '0',
+            left: '0',
+            width: '50%',
+            height: '200px',
+            zIndex: 2,
+            borderRadius:"10px",
+            boxShadow: '0 10px 30px var(--secondary)'
+        }}
+    />
+</div>
                 </div>
             </div>
         </section>
---
+++ src/pages/homePage/homeComponent/Cta.tsx
@@ -0,0 +1,9 @@
+export const Cta = ()=>{
+    return(
+        <section>
+            <div className="container-fluid">
+                <p className="homeBadge">Get in touch</p>
+            </div>
+        </section>
+    )
+}
